d129db02561e304f436b9d07ad0c1f7ff9d53043
guilhembn <gbuisan@laas.fr>
2021-09-26 17:18:33 +0200

    [RotationControl] Implements rotation control

--- src/RotationControl.cpp
+++ src/RotationControl.cpp
@@ -5,5 +5,36 @@ namespace rd {
 RotationControl::RotationControl(){};
 
 Speed RotationControl::computeSpeed(const PointOriented &robotPose, const Speed &robotSpeed, double dt) {
+  Angle diff = targetAngle_ - robotPose.theta();
+  double acceleration;
+  if (diff < 0) {
+    acceleration = -param<double>(MAX_ROTATIONAL_ACCELERATION);
+  } else {
+    acceleration = param<double>(MAX_ROTATIONAL_ACCELERATION);
+  }
+  double durationToStop = robotSpeed.vtheta() / acceleration;
+
+  Angle plannedStopAngle = robotPose.theta() + Angle(2 * (robotSpeed.vtheta() * durationToStop - 0.5 * acceleration * durationToStop * durationToStop));
+  Angle plannedError = targetAngle_ - plannedStopAngle;
+  double speedCommand;
+  if (std::abs(plannedError.value()) <= 0.05 || diff.value() * plannedError.value() < 0.0) {  // If the next error is smaller than the acceptable error, or if we will overshoot the target angle
+    double speedDiff = acceleration * dt;
+    if (std::abs(speedDiff) > std::abs(robotSpeed.vtheta())) {  // The acceleration to stop is less than max acceleration, so stop
+      speedCommand = 0.0;
+    } else {
+      speedCommand = robotSpeed.vtheta() - speedDiff;  // Apply max acceleration to brake
+    }
+  } else if (std::abs(plannedError.value()) <= 3 * 0.05 && std::abs(robotSpeed.vtheta()) >= 0.3) {
+    // If we next loop will not be too far from target, and the robot is moving at an acceptable speed, do not brake, do not accelerate
+    speedCommand = robotSpeed.vtheta();
+  } else {
+    // We still have path to travel
+    double mul = 1.;
+    if (std::abs(diff.value()) <= 3 * 0.05) {
+      mul = 0.2;
+    }
+    speedCommand = robotSpeed.vtheta() + dt * acceleration * mul;
+  }
+  return Speed(0.0, 0.0, speedCommand);
 }
 }  // namespace rd
--- tests/navigation.cpp
+++ tests/navigation.cpp
@@ -2,12 +2,22 @@
 
 #include "Navigation/Geometry/Point.h"
 #include "Navigation/Geometry/Trajectory.h"
+#include "Navigation/PositionControlBase.h"
+#include "Navigation/RotationControl.h"
 
 void say_hello() { std::cout << "Hello, from navigation!\n"; }
 
 int main() {
   say_hello();
-  rd::Trajectory t;
-  rd::Point *p = new rd::PointOriented(10., 9., 2.4);
-  std::cout << *static_cast<rd::PointOriented *>(p) << std::endl;
+  rd::PointOriented robotPose(1.0, 0.0, 1.5);
+  rd::Speed robotSpeed(2.0, 0.0, 0.0);
+  rd::RotationControl rc;
+  rc.setParam(rd::MAX_ROTATIONAL_ACCELERATION, 3.0);
+  rc.setTargetAngle(2.0);
+  for (size_t i = 0; i < 100; i++) {
+    rd::Speed speed = rc.computeSpeed(robotPose, robotSpeed, 0.1);
+    robotPose += rd::PointOriented(0.0, 0.0, speed.vtheta() * 0.1);
+    std::cout << speed << std::endl;
+    std::cout << robotPose << std::endl;
+  }
 }
