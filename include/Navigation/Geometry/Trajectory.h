36eee68d1b689072aaa657faa11f219e8fb1fe3c
guilhembn <gbuisan@laas.fr>
2021-09-27 10:57:18 +0200

    [Geometry] Adds geometry functions to Trajectory

--- include/Navigation/Geometry/Trajectory.h
+++ include/Navigation/Geometry/Trajectory.h
@@ -1,8 +1,9 @@
 #ifndef TRAJECTORY_H
 #define TRAJECTORY_H
-#include "Navigation/Geometry/Point.h"
 #include <deque>
 #include <vector>
+
+#include "Navigation/Geometry/Point.h"
 
 namespace rd {
 
@@ -12,17 +13,48 @@ struct PointOrientedSpeed {
 };
 
 class Trajectory {
-public:
+ public:
   Trajectory();
   Trajectory(const std::vector<PointOriented> &points);
+  /**
+   * @brief Finds the closest point on the trajectory to the point given. The returned point is the linear interpolation of factor tOut
+   * between the points at closestPrevIndex and closestPrevIndex + 1. The search stops if a point on the trajectory has a null speed.
+   *
+   * @param point The point to search for the closest point on the trajectory
+   * @param tOut The interpolation factor
+   * @param closestPrevIndex The index of the previous point of the trajectory
+   * @return Point The closest point on the trajectory.
+   * (Point = traj.at(closestPrevIndex) + tOut * (traj.at(closestPrevIndex + 1) - traj.at(closestPrevIndex)))
+   */
+  Point pointWithSpeedClosestTo(const Point &point, double &tOut, size_t &closestPrevIndex) const;
+  /**
+   * @brief Finds the closest point on the trajectory to the given point. The search stops when the distance to the point increases (In case of trajectory crossings).
+   *
+   * @param point The point to search for the closest point on the trajectory
+   * @param tOut The interpolation factor
+   * @param closestPrevIndex The index of the previous point of the trajectory
+   * @return Point The closest point on the trajectory
+   * (Point = traj.at(closestPrevIndex) + tOut * (traj.at(closestPrevIndex + 1) - traj.at(closestPrevIndex)))
+   */
+  Point nextPointClosestTo(const Point &point, double &tOut, size_t &closestPrevIndex) const;
+
+  /**
+   * @brief Finds the point along the trajectory at 'distance' from 'pointStart'. First finds the closest point on the trajectory,
+   * then browse the trajectory until distance or a point with null speed is reached.
+   *
+   * @param distance the distance along the trajectory
+   * @param pointStart the starting point (will be projected on the trajectory)
+   * @return Point the point on the trajectory at a distance from the projection of pointStart on the trajectory
+   */
+  Point pointAtDistanceFrom(const double distance, const Point &pointStart);
 
   void pop() { pointspeeds_.pop_front(); };
   const PointOrientedSpeed &at(size_t i) const;
   size_t size() const { return pointspeeds_.size(); };
 
-protected:
+ protected:
   std::deque<PointOrientedSpeed> pointspeeds_;
 };
-} // namespace rd
+}  // namespace rd
 
 #endif /* TRAJECTORY_H */
--- src/Geometry/Trajectory.cpp
+++ src/Geometry/Trajectory.cpp
@@ -1,10 +1,86 @@
 #include "Navigation/Geometry/Trajectory.h"
 
-namespace rd {
-Trajectory::Trajectory() : pointspeeds_({}){};
+#include <limits>
 
-Trajectory::Trajectory(const std::vector<PointOriented> &points) {}
+namespace rd {
+Trajectory::Trajectory() : pointspeeds_({}) {}
+
+Trajectory::Trajectory(const std::vector<PointOriented> &) {}
 
 const PointOrientedSpeed &Trajectory::at(size_t i) const { return pointspeeds_.at(i); }
 
-} // namespace rd
+Point Trajectory::pointWithSpeedClosestTo(const Point &point, double &tOut, size_t &closestPrevIndex) const {
+  double distMin = std::numeric_limits<double>::max();
+  double tMin = 0.;
+  size_t iMin = -1;
+  Point pointMin;
+  for (size_t i = 0; i < size() - 1; i++) {
+    double t;
+    Point pt = point.closestPointBetween(pointspeeds_.at(i).point, pointspeeds_.at(i + 1).point, t);
+    double dist = pt.squaredDistanceTo(point);
+    if (dist < distMin) {
+      distMin = dist;
+      tMin = t;
+      iMin = i;
+      pointMin = pt;
+    }
+    if (i != 0 && pointspeeds_.at(i).speed == 0.0) {
+      break;
+    }
+  }
+  tOut = tMin;
+  closestPrevIndex = iMin;
+  return pointMin;
+}
+
+Point Trajectory::nextPointClosestTo(const Point &point, double &tOut, size_t &closestPrevIndex) const {
+  double distMin = std::numeric_limits<double>::max();
+  double tMin = 0.;
+  size_t iMin = -1;
+  Point pointMin;
+  for (size_t i = 0; i < size() - 1; i++) {
+    double t;
+    Point pt = point.closestPointBetween(pointspeeds_.at(i).point, pointspeeds_.at(i + 1).point, t);
+    double dist = pt.squaredDistanceTo(point);
+    if (dist < distMin) {
+      distMin = dist;
+      tMin = t;
+      iMin = i;
+      pointMin = pt;
+    }
+    if (dist > distMin) {
+      break;
+    }
+  }
+  tOut = tMin;
+  closestPrevIndex = iMin;
+  return pointMin;
+}
+
+Point Trajectory::pointAtDistanceFrom(const double distance, const Point &pointStart) {
+  double t;
+  size_t previousIndex;
+  Point proj = nextPointClosestTo(pointStart, t, previousIndex);
+
+  double distanceLeft = distance;
+  double pathLen = pointspeeds_.at(previousIndex + 1).point.distanceTo(proj);
+  size_t browsingTraj = previousIndex;
+  while (distanceLeft > pathLen) {  // while the distance left is greater than the length of a full segment
+    if (pointspeeds_.at(browsingTraj + 1).speed == 0.0) {
+      // If the robot must stop on this point, do not explore the rest, manage to get it there
+      t = 0.;
+      distanceLeft = (pointspeeds_.at(browsingTraj + 1).point - pointspeeds_.at(browsingTraj).point).norm();
+      break;
+    }
+    t = 0.;
+    browsingTraj++;
+    distanceLeft -= pathLen;
+    pathLen = (pointspeeds_.at(browsingTraj + 1).point - pointspeeds_.at(browsingTraj).point).norm();
+  }
+  Point ab = pointspeeds_.at(browsingTraj + 1).point - pointspeeds_.at(browsingTraj).point;
+  double tGoal = t + distanceLeft / ab.norm();
+  Point goal = (Point)pointspeeds_.at(browsingTraj).point + ab * tGoal;
+  return goal;
+}
+
+}  // namespace rd
