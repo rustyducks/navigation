abda09981e96feeaa18e8ddc16c1abf119f4b118
guilhembn <gbuisan@laas.fr>
2021-10-03 22:14:09 +0200

    Fixes Rotation Control

--- src/RotationControl.cpp
+++ src/RotationControl.cpp
@@ -17,7 +17,7 @@ Speed RotationControl::computeSpeed(const PointOriented &robotPose, const Speed 
   Angle plannedStopAngle = robotPose.theta() + Angle(2 * (robotSpeed.vtheta() * durationToStop - 0.5 * acceleration * durationToStop * durationToStop));
   Angle plannedError = targetAngle_ - plannedStopAngle;
   double speedCommand;
-  if (std::abs(plannedError.value()) <= 0.05 ||
+  if (std::abs(plannedError.value()) <= param<double>(ADMITTED_ANGLE_POSITION_ERROR) ||
       diff.value() * plannedError.value() < 0.0) {  // If the next error is smaller than the acceptable error, or if we will overshoot the target angle
     double speedDiff = acceleration * dt;
     if (std::abs(speedDiff) > std::abs(robotSpeed.vtheta())) {  // The acceleration to stop is less than max acceleration, so stop
@@ -25,17 +25,18 @@ Speed RotationControl::computeSpeed(const PointOriented &robotPose, const Speed 
     } else {
       speedCommand = robotSpeed.vtheta() - speedDiff;  // Apply max acceleration to brake
     }
-  } else if (std::abs(plannedError.value()) <= 3 * 0.05 && std::abs(robotSpeed.vtheta()) >= 0.3) {
+  } else if (std::abs(plannedError.value()) <= 3. * param<double>(ADMITTED_ANGLE_POSITION_ERROR) && std::abs(robotSpeed.vtheta()) >= 0.3) {
     // If we next loop will not be too far from target, and the robot is moving at an acceptable speed, do not brake, do not accelerate
     speedCommand = robotSpeed.vtheta();
   } else {
     // We still have path to travel
     double mul = 1.;
-    if (std::abs(diff.value()) <= 3 * 0.05) {
-      mul = 0.2;
+    if (std::abs(diff.value()) <= 3. * param<double>(ADMITTED_ANGLE_POSITION_ERROR)) {
+      mul = 0.7;
     }
     speedCommand = robotSpeed.vtheta() + dt * acceleration * mul;
   }
+  speedCommand = std::min(param<double>(MAX_ROTATIONAL_SPEED), std::max(-param<double>(MAX_ROTATIONAL_SPEED), speedCommand));
   return Speed(0.0, 0.0, speedCommand);
 }
 }  // namespace rd
