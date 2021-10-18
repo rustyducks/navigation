82472af6e195d6a8d305e0ba51a44c4597185623
guilhembn <gbuisan@laas.fr>
2021-10-18 22:27:31 +0200

    [PurePursuit] Adds cruise state

--- src/PurePursuitControl.cpp
+++ src/PurePursuitControl.cpp
@@ -1,7 +1,18 @@
 #include "Navigation/PurePursuitControl.h"
 
+#include "Navigation/Communication/Ivy.h"
+
 namespace rd {
-PurePursuitControl::PurePursuitControl() : trajectory_() { linearControl_.setParam(IS_HOLONOMIC, false); }
+PurePursuitControl::PurePursuitControl() : trajectory_() {
+  linearControl_.setParam(IS_HOLONOMIC, false);
+  rotationControl_.setParam(MAX_ROTATIONAL_SPEED, 1.0);  // Todo (guilhembn): Do not pass parameters like so...
+  rotationControl_.setParam(MAX_ROTATIONAL_ACCELERATION, 0.1);
+  rotationControl_.setParam(ADMITTED_ANGLE_POSITION_ERROR, 0.05);
+  linearControl_.setParam(ParamName::MAX_LINEAR_ACCELERATION, 10.);
+  linearControl_.setParam(ParamName::MAX_LINEAR_SPEED, 100.);
+  linearControl_.setParam(ADMITTED_LINEAR_POSITION_ERROR, 5.);
+  linearControl_.setParam(LINEAR_CONTROL_STOP_DISTANCE_FACTOR, 2.);
+}
 
 Speed PurePursuitControl::computeSpeed(const PointOriented& robotPose, const Speed& robotSpeed, double dt) {
   Angle targetAngle;
@@ -24,7 +35,7 @@ Speed PurePursuitControl::computeSpeed(const PointOriented& robotPose, const Spe
 
     case PurePursuitState::CRUISING:
 
-      return Speed(0.0, 0.0, 0.0);
+      return cruising(robotPose, robotSpeed, dt);
       break;
     case PurePursuitState::LAST_ROTATION:
       if (std::abs(robotSpeed.vtheta()) <= 0.01 && std::abs((trajectory_.at(0).point.theta() - robotPose.theta()).value()) < 0.05) {
@@ -43,26 +54,37 @@ Speed PurePursuitControl::computeSpeed(const PointOriented& robotPose, const Spe
 }
 
 Speed PurePursuitControl::cruising(const PointOriented& robotPose, const Speed& robotSpeed, double dt) {
-  PointOriented nextTrajPoint = trajectory_.at(1).point;  // trajectory.at(0) is past point, in order for the interpolation to work
-  double nextTrajSpeed = trajectory_.at(1).speed;
+  PointOriented nextTrajPoint = trajectory_.at(trajectoryCurrentIndex_).point;  // trajectory.at(0) is past point, in order for the interpolation to work
+  double nextTrajSpeed = trajectory_.at(trajectoryCurrentIndex_).speed;
   double positionError = robotPose.distanceTo(nextTrajPoint);
+  Ivy::getInstance().sendPoint(5, nextTrajPoint);
   if (positionError < param<double>(ADMITTED_LINEAR_POSITION_ERROR)) {
     // If we are not too far
-    if (trajectory_.size() == 1) {
+    if (trajectoryCurrentIndex_ == trajectory_.size() - 1) {
       // If it is the last point of the trajectory, start final rotation
+      for (size_t i = 0; i < trajectoryCurrentIndex_; i++) {
+        trajectory_.pop();
+      }
+      trajectoryCurrentIndex_ = 1;
       state_ = PurePursuitState::LAST_ROTATION;
       rotationControl_.setTargetAngle(nextTrajPoint.theta());
       return rotationControl_.computeSpeed(robotPose, robotSpeed, dt);
     } else if (nextTrajSpeed < 0.1) {
       // If it is not the last point, but we should stop, start a new initial rotation
-      trajectory_.pop();
+      for (size_t i = 0; i < trajectoryCurrentIndex_; i++) {
+        trajectory_.pop();
+      }
+      trajectoryCurrentIndex_ = 1;
       state_ = PurePursuitState::FIRST_ROTATION;
       Angle targetAngle = robotPose.angleTo(trajectory_.at(1).point);
       rotationControl_.setTargetAngle(targetAngle);
       return rotationControl_.computeSpeed(robotPose, robotSpeed, dt);
     } else {
       // Go to the next point in cruising
-      trajectory_.pop();
+      for (size_t i = 0; i < trajectoryCurrentIndex_; i++) {
+        trajectory_.pop();
+      }
+      trajectoryCurrentIndex_ = 1;
       return purePursuit(robotPose, robotSpeed, dt);
     }
   } else {
