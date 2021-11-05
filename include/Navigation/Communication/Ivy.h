6cb1b6377eb9325fe5ac386cbdca8821621cacbe
guilhembn <gbuisan@laas.fr>
2021-11-05 12:29:57 +0100

    [Ivy] Adds subscription to Ivy msgs

--- include/Navigation/Communication/Ivy.h
+++ include/Navigation/Communication/Ivy.h
@@ -3,6 +3,7 @@
 #include <thread>
 
 #include "GeometryTools/Point.h"
+#include "GeometryTools/Speed.h"
 #include "GeometryTools/Trajectory.h"
 
 namespace rd {
@@ -17,11 +18,25 @@ class Ivy {
   void sendPoint(const size_t id, const Point& point) const;
   void sendRobotPose(const PointOriented& robotPose) const;
 
+  void subscribeGoToOrient(const std::function<void(const PointOriented&)>& onGoToOrient);
+  void subscribeGoTo(const std::function<void(const Point&)>& onGoTo);
+  void subscribeCustomAction(const std::function<void(const int)>& onCustomAction);
+  void subscribeSpeedDirection(const std::function<void(const Speed&)>& onSpeedDirection);
+
+  void onGoToOrientBase(int argc, char** argv) const;
+  void onGoToBase(int argc, char** argv) const;
+  void onCustomActionBase(int argc, char** argv) const;
+  void onSpeedDirectionBase(int argc, char** argv) const;
+
  protected:
   ~Ivy();
   Ivy();
   Ivy(const Ivy&);
   void operator=(const Ivy&);
+  std::vector<std::function<void(const PointOriented&)>> onGoToOrientCbs_;
+  std::vector<std::function<void(const Point&)>> onGoToCbs_;
+  std::vector<std::function<void(const int)>> onCustomActionCbs_;
+  std::vector<std::function<void(const Speed&)>> onSpeedDirectionCbs_;
   std::thread ivyThread_;
 };
 }  // namespace rd
--- src/Communication/Ivy.cpp
+++ src/Communication/Ivy.cpp
@@ -45,4 +45,88 @@ void Ivy::sendRobotPose(const PointOriented& robotPose) const {
   oss << "Update robot pose " << robotPose.x() << ";" << robotPose.y() << ";" << robotPose.theta().value();
   IvySendMsg("%s", oss.str().c_str());
 }
+
+void Ivy::subscribeGoToOrient(const std::function<void(const PointOriented&)>& onGoToOrient) {
+  if (onGoToOrientCbs_.size() == 0) {
+    IvyBindMsg([](const IvyClientPtr, void*, int argc, char** argv) -> void { Ivy::getInstance().onGoToOrientBase(argc, argv); }, nullptr,
+               "Go to orient (.*),(.*),(.*)");
+  }
+  onGoToOrientCbs_.push_back(onGoToOrient);
+}
+
+void Ivy::onGoToOrientBase(int argc, char** argv) const {
+  if (argc != 3) {
+    std::cout << "[Ivy] Received an ill formed 'GoToOrient' request. Discarding it." << std::endl;
+  }
+  double x, y, theta;
+  sscanf(argv[0], "%lf", &x);
+  sscanf(argv[1], "%lf", &y);
+  sscanf(argv[2], "%lf", &theta);
+  PointOriented p(x, y, theta);
+  for (const auto cb : onGoToOrientCbs_) {
+    cb(p);
+  }
+}
+
+void Ivy::subscribeGoTo(const std::function<void(const Point&)>& onGoTo) {
+  if (onGoToCbs_.size() == 0) {
+    IvyBindMsg([](const IvyClientPtr, void*, int argc, char** argv) -> void { Ivy::getInstance().onGoToBase(argc, argv); }, nullptr, "Go to linear (.*),(.*)");
+  }
+  onGoToCbs_.push_back(onGoTo);
+}
+
+void Ivy::onGoToBase(int argc, char** argv) const {
+  if (argc != 2) {
+    std::cout << "[Ivy] Received an ill formed 'GoTo' request. Discarding it." << std::endl;
+  }
+  double x, y;
+  sscanf(argv[0], "%lf", &x);
+  sscanf(argv[1], "%lf", &y);
+  Point p(x, y);
+  for (const auto cb : onGoToCbs_) {
+    cb(p);
+  }
+}
+
+void Ivy::subscribeCustomAction(const std::function<void(const int)>& onCustomAction) {
+  if (onCustomActionCbs_.size() == 0) {
+    IvyBindMsg([](const IvyClientPtr, void*, int argc, char** argv) -> void { Ivy::getInstance().onCustomActionBase(argc, argv); }, nullptr,
+               "Custom action (.*)");
+  }
+  onCustomActionCbs_.push_back(onCustomAction);
+}
+
+void Ivy::onCustomActionBase(int argc, char** argv) const {
+  if (argc != 1) {
+    std::cout << "[Ivy] Received an ill formed 'CustomAction' request. Discarding it." << std::endl;
+  }
+  int ca;
+  sscanf(argv[0], "%d", &ca);
+  for (const auto cb : onCustomActionCbs_) {
+    cb(ca);
+  }
+}
+
+void Ivy::subscribeSpeedDirection(const std::function<void(const Speed&)>& onSpeedDirection) {
+  if (onSpeedDirectionCbs_.size() == 0) {
+    IvyBindMsg([](const IvyClientPtr, void*, int argc, char** argv) -> void { Ivy::getInstance().onSpeedDirectionBase(argc, argv); }, nullptr,
+               "Direction (.*),(.*),(.*)");
+  }
+  onSpeedDirectionCbs_.push_back(onSpeedDirection);
+}
+
+void Ivy::onSpeedDirectionBase(int argc, char** argv) const {
+  if (argc != 3) {
+    std::cout << "[Ivy] Received an ill formed 'SpeedDirection' request. Discarding it." << std::endl;
+  }
+  double vx, vy, vtheta;
+  sscanf(argv[0], "%lf", &vx);
+  sscanf(argv[1], "%lf", &vy);
+  sscanf(argv[2], "%lf", &vtheta);
+  Speed s(vx, vy, vtheta);
+  for (const auto cb : onSpeedDirectionCbs_) {
+    cb(s);
+  }
+}
+
 }  // namespace rd
