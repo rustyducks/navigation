cad55935f60d57dd3fc669171c9ef9ad9316d51d
guilhembn <gbuisan@laas.fr>
2021-12-06 13:57:10 +0100

    Adds minimum speed fields to parameters

--- include/Navigation/Parameters.h
+++ include/Navigation/Parameters.h
@@ -4,8 +4,10 @@
 #define IS_HOLONOMIC false
 #define MAX_LINEAR_ACCELERATION 50.         // mm/s²
 #define MAX_LINEAR_SPEED 200.               // mm/s
+#define MIN_LINEAR_SPEED 10.                // mm/s
 #define MAX_ROTATIONAL_ACCELERATION 0.5     // rad/s²
 #define MAX_ROTATIONAL_SPEED 1.8            // rad/s
+#define MIN_ROTATIONAL_SPEED 0.2            // rad/s
 #define ADMITTED_LINEAR_POSITION_ERROR 5.   // mm
 #define ADMITTED_ANGLE_POSITION_ERROR 0.05  // rad
 
@@ -21,6 +23,8 @@ struct PositionControlParameters {
   double maxRotationalSpeed = MAX_ROTATIONAL_SPEED;
   double admittedLinearPositionError = ADMITTED_LINEAR_POSITION_ERROR;
   double admittedAnglePositionError = ADMITTED_ANGLE_POSITION_ERROR;
+  double minLinearSpeed = MIN_LINEAR_SPEED;
+  double minRotationalSpeed = MIN_ROTATIONAL_SPEED;
 };
 }  // namespace rd
 #endif /* PARAMETERS_H */
